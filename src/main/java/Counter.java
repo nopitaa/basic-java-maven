1784f475e1589a48c258cbac4dd0d3e2d4bf057c
nopitaa <novitasyahwahapsari@gmail.com>
2026-05-09 00:12:27 +0700

    initial commit

---
+++ src/main/java/Counter.java
@@ -0,0 +1,26 @@
+
+public class Counter {
+
+	private int count;
+	
+	public Counter() {
+		this.reset();
+	}
+		
+	public void reset() {
+		count = 0;
+	}
+	
+	public void increment() {
+		count++;
+	}
+	
+	public void decrement() {
+		count--;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+}
