d40e086ef1cf65d73e0107344f46485766f1e4f3
nopitaa <novitasyahwahapsari@gmail.com>
2026-05-09 00:20:49 +0700

    coba

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -7,7 +7,7 @@ public class CounterTest {
 	public void testReset() {
 		Counter testCounter = new Counter();
 		
-		for(int i = 0; i <10; i++) {
+		for(int i = 0; i <10; i+) {
 			testCounter.increment();
 		}
 		
